09cf64b7e2f5a646968b073154d3419d487a24d3
NiravJD <128644217+NiravJD@users.noreply.github.com>
2023-03-23 13:27:07 +0000

    Add files via upload
    Recently, I was asked about Value at Risk (VaR) and thought of implementing an example in Python. The code here illustrates stressed VaR, which is a 10 day (holding period) unweighted (all points in time series treated equally) VaR for a 99% confidence interval.
    
    The time series used is for Turkish Lira FX rate during the 2008 period.
    
    It’s a fairly simple exercise.
    
    However, the points to ponder are:
    
    What if a timeseries in not available? E.g. EUR in 2000
    What if the timeseries is discontinuous? E.g. FX peg break.
    How to consider restructurings, takeover and succession events?

---
+++ SVaR.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 13 15:41:14 2023
+
+@author: ndumasw1
+"""
+
+#Importing necessary modules
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Reading input file
+FX = pd.read_excel(r"V:\FAO\RMM\LON\MarketRiskEMG\EMG\Nirav\Python\TRY SVaR.xlsx")
+
+
+# 1)Calculate 10 day returns
+# 2)Multiply by risk i.e. FX delta in this case
+
+FX['daily_return'] = (FX['USDTRY']/FX['USDTRY'].shift(-10)) - 1 
+FX['PnL'] = FX['daily_return'] * -1000000 
+
+# Remove the last 10 rows as not required 
+FX.drop(FX.tail(10).index, inplace = True) 
+
+#Sort PnL ascending
+FX.sort_values(by=['PnL'], ascending=True, inplace=True)
+
+# Isolating 99% confidence interval, which is 98% expected shortfall
+
+FX['Driver Date'] = FX['Date'].head(5)
+
+FX['VaR Tail'] = FX['PnL'].head(5)
+
+SVaR = (FX['VaR Tail']).mean()
+
+print(SVaR)
+
+FX.to_excel(r"V:\FAO\RMM\LON\MarketRiskEMG\EMG\Nirav\Python\Modified TRY SVaR.xlsx")
